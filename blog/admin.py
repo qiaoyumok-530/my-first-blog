cdec8b6aa7324cc5a6dc0f65e07b729a6790d6f4
Your Name <you@example.com>
2025-11-01 16:09:44 +0800

    My Django Girls app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Post
+# To delete, edit and delte the posts that we modeled
+admin.site.register(Post)
+
+# Register your models here.
+
---
+++ test.py
@@ -0,0 +1,16 @@
+#inside the bracket is PARAMETER
+"""def hi(name):
+    if name == "Ola":
+        print("Hi Ola")
+    elif name == "Sonja":
+        print("Hi Sonja!")
+    else:
+        print("Hi " + name +"!")
+#hi("fish")
+girls = ["Yu", "Wen", "Gene"]
+for name in girls:
+    hi(name)
+    print("next girl!")"""
+
+for i in range(1,6):
+    print(i)
